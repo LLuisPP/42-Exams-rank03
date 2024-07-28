3af9d77c9c40626af1b410334b54a9096417a4f0
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2024-07-28 19:29:52 +0200

    ft_printf update

--- ft_printf/mine.c
+++ ft_printf/mine.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   mine.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
+/*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 15:59:09 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/07/27 16:54:55 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/07/28 19:29:27 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdarg.h>
 
-void	put_str(char *str, int *len)
+void	ft_putstr(char *str, int *len)
 {
 	if (!str)
 		str = "(null)";
@@ -25,7 +25,7 @@ void	put_str(char *str, int *len)
 	}
 }
 
-void	put_digit(long long int nbr, int base, int *len)
+void	ft_putdigit(long long int nbr, int base, int *len)
 {
 	char	*hex = "0123456789abcdef";
 
@@ -36,34 +36,36 @@ void	put_digit(long long int nbr, int base, int *len)
 		write(1, "-", 1);
 	}
 	if (nbr >= base)
-		put_digit((nbr / base), base, len);
+		ft_putdigit((nbr / base), base, len);
 	*len += 1;
 	write(1, &hex[nbr % base], 1);
 }
 
-int	ft_printf(const char *fmt, ... )
+int ft_printf(const char *fmt, ... )
 {
-	int len = 0;
+	int	len;
 
-	va_list	ptr;
+	len = 0;
+	va_list ptr;
 	va_start(ptr, fmt);
-
 	while (*fmt)
 	{
-		if ((*fmt == '%') && ((*(fmt + 1) == 's') || (*(fmt + 1) == 'd') || (*(fmt + 1) == 'x')))
+		if(*fmt == '%' && (*(fmt + 1) == 's' || *(fmt + 1) == 'd' || *(fmt + 1) == 'x'))
 		{
 			fmt++;
 			if (*fmt == 's')
-				put_str(va_arg(ptr, char *), &len);
+				ft_putstr(va_arg(ptr, char *), &len);
 			else if (*fmt == 'd')
-				put_digit((long long int)va_arg(ptr, int), 10, &len);
+				ft_putdigit((long long int)va_arg(ptr, int), 10, &len);
 			else if (*fmt == 'x')
-				put_digit((long long int)va_arg(ptr, unsigned int), 16, &len);
+				ft_putdigit((long long int)va_arg(ptr, unsigned int), 16, &len);
 		}
 		else
-			len += write(1, fmt, 1);
+		{
+			len += 1;
+			write(1, fmt, 1);
+		}
 		fmt++;
 	}
-	return (va_end(ptr), len);
+	return (va_end(ptr),len);
 }
-
